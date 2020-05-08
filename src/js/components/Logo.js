8dde293dc7e30fe2d14b19ecc3588cb757d27ee1
Arturas Lapinskas <arturas@Arturass-MacBook-Pro.local>
2020-05-08 06:35:24 +0300

    fixed changing name

--- src/js/components/Logo.js
+++ src/js/components/Logo.js
@@ -1,29 +1,54 @@
 ﻿import React from 'react';
-import { FormGroup, Classes, Dialog } from "@blueprintjs/core";
+import { FormGroup, Classes, Dialog, Button } from "@blueprintjs/core";
 
 class Logo extends React.Component{
 
     constructor(props){
         super();
         this.dataStore = props.store;
+        const authorName = this.dataStore.getAuthorName();
         this.state = {
-            authorname: this.dataStore.getAuthorName(),
+            authorName: authorName,
+            inputName: authorName,
             isDialogOpen: false
         };
-        this._updateAuthorName = this._updateAuthorName.bind(this)
+        this._updateInputName = this._updateInputName.bind(this);
+        this._updateAuthorName = this._updateAuthorName.bind(this);
+        this._showChangeNameDialog = this._showChangeNameDialog.bind(this);
+        this._saveAndClose = this._saveAndClose.bind(this);
+        this._cancelAndClose = this._cancelAndClose.bind(this);
+        this._closeChangeNameDialog = this._closeChangeNameDialog.bind(this);
     }
 
     componentWillMount() {
         this.dataStore.on('author', this._updateAuthorName);
-        //this.setState({authorname: this.dataStore.getAuthorName()});
     }
 
-    _updateAuthorName(authorname) {
-        this.setState({authorname});
+    _updateInputName(inputName) {
+        this.setState({inputName});
+    }
+
+    _updateAuthorName(authorName) {
+        this.setState({authorName});
+        this._updateInputName(authorName);
     }
 
     _showChangeNameDialog() {
         this.setState({ isDialogOpen: true });
+    }
+
+    _cancelAndClose() {
+        this._updateAuthorName(this.state.authorName);
+        this._closeChangeNameDialog();
+    }
+
+    _saveAndClose() {
+        this.dataStore.setAuthorName(this.state.inputName);
+        this._closeChangeNameDialog();
+    }
+
+    _closeChangeNameDialog() {
+        this.setState({ isDialogOpen: false });
     }
 
     render() {
@@ -31,13 +56,18 @@ class Logo extends React.Component{
             <div className="header">
                 <div className="Logo" />
                 <FormGroup className="user-info">
-                    <p>Вы вошли как: {this.state.authorname} </p>
-                    <input onClick={() => this._showChangeNameDialog()} class="bp3-button" type="submit" value="Сменить ник" />
+                    <p>Вы вошли как: {this.state.authorName} </p>
+                    <input onClick={this._showChangeNameDialog} class="bp3-button" type="submit" value="Сменить ник" />
                 </FormGroup>
                 <Dialog icon="info-sign" canEscapeKeyClose={true} isCloseButtonShown={true} isOpen={this.state.isDialogOpen}>
                     <FormGroup className={Classes.DIALOG_BODY}>
-                        <input className="shout_box bp3-input" value={this.state.authorname} onChange={(input) => this._updateAuthorName(input.target.value)} type="text"/>
-                        <input class="bp3-button" type="submit" value="Сохранить" />
+                        <input className="shout_box bp3-input" value={this.state.inputName} onChange={(input) => this._updateInputName(input.target.value)} type="text"/>
+                        <div className={Classes.DIALOG_FOOTER}>
+                            <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                                <Button onClick={this._cancelAndClose}>Отмена</Button>
+                                <Button onClick={this._saveAndClose}>Сохранить</Button>
+                            </div>
+                        </div>
                     </FormGroup>
                 </Dialog>
             </div>
