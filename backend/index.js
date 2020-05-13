2817fc8907cee6e7487c84f95a13416f0762642f
Arturas Lapinskas <arturas@Arturass-MacBook-Pro.local>
2020-05-13 17:25:12 +0300

    fixed postgres connection

--- backend/index.js
+++ backend/index.js
@@ -115,6 +115,9 @@ function connectToDb() {
               }*/
               //https.globalAgent.options.rejectUnauthorized = false;
               //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+              console.log(process.env.DATABASE_URL);
+              console.log('-------------');
+              console.log(Pool);
               db = new Pool({
                 connectionString: process.env.DATABASE_URL,
                 //connectionString: "postgres://hjpvdkqvhuucpl:5bcaa8bfb4e3c1b11d4defd81b71cf9a79eff0dcf838b1871e2f464b888fd9de@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/d7me8q5bctljau",
